8397c03e21ac92af2cef04fd1da9755883ef6b7e
bryonybailey485 <bryony.bailey485@gmail.com>
2025-05-15 17:37:56 +0100

    Started to code the first question and made the buttons

---
+++ Assets/js/script.js
@@ -0,0 +1,14 @@
+const wrapper = document.getElementById("wrapper");
+const question = document.getElementById("question");
+const answer0 = document.getElementById("answer0");
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
+const answer3 = document.getElementById("answer3");
+const score = document.getElementById("score");
+
+const questions = [
+    {
+        "question": "What planet is closest to the sun?",
+        "answers":["Mars", "Mercury", "Jupiter", "Venus"] // Mercury
+    }
+]
